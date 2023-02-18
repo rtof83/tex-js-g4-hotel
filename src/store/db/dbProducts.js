846647329cc4ca2205d7140d62db6cdfcde3375d
mauricio-alves <mauricioalvesnutri@gmail.com>
2023-02-18 14:26:03 -0300

    feat: pagina relatorio de consumo

---
+++ src/store/db/dbProducts.js
@@ -0,0 +1,23 @@
+const dbProducts = {
+  frigobar: [
+    { name: "Água", qty: 0, price: 2.5 },
+    { name: "Cerveja", qty: 0, price: 5.0 },
+    { name: "Refrigerante", qty: 0, price: 4.0 },
+  ],
+  cesta: [
+    { name: "Barra de chocolate", qty: 0, price: 3.0 },
+    { name: "Batata Chips", qty: 0, price: 6.0 },
+    { name: "Amendoim", qty: 0, price: 4.5 },
+  ],
+  bar: [
+    { name: "Café", qty: 0, price: 2.0 },
+    { name: "Pão de Queijo", qty: 0, price: 3.5 },
+    { name: "Água", qty: 0, price: 2.5 },
+    { name: "Lanche", qty: 0, price: 8.0 },
+    { name: "Almoço", qty: 0, price: 15.0 },
+    { name: "Cerveja", qty: 0, price: 5.0 },
+    { name: "Refrigerante", qty: 0, price: 4.0 },
+  ],
+};
+
+export default dbProducts;
