762069ce1c915d204e5342acb31bc3e21c79e752
rtof <zuichuan@msn.com>
2023-02-27 19:07:21 -0300

    improving mysql2 connection and adding get accommodation by id endpoint

--- admin/api/database/conn.js
+++ admin/api/database/conn.js
@@ -1,42 +1,56 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const app = express();
+
 require('dotenv').config();
 app.use(express.json());
 
+// mysql2
+async function connect(){
+  if(global.connection && global.connection.state !== 'disconnected')
+    return global.connection;
 
-// mysql2 test
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'admin',
-  password: 'admin123',
-  database: 'hotel'
-});
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  });
+
+  app.listen(process.env.APP_PORT, () => 
+    console.log(`listening on port ${process.env.APP_PORT}...`));
+
+  console.log('Connection has been established successfully. (mysql2)');
+  global.connection = connection;
+  return connection;
+};
 // ===========================
 
 
 // sequelize
-const conn = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT
-});
+// const conn = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//   dialect: process.env.DB_DIALECT,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT
+// });
 // ===========================
 
 
-(async () => {
-  await conn.sync();
-})();
+// (async () => {
+//   await conn.sync();
+// })();
 
-conn
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-    app.listen(process.env.APP_PORT);
-  })
-  .catch((err) => {
-    console.log('Unable to connect to the database:', err);
-  });
+// conn
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//     app.listen(process.env.APP_PORT);
+//   })
+//   .catch((err) => {
+//     console.log('Unable to connect to the database:', err);
+//   });
 
-module.exports = { app, conn, connection };
+// module.exports = { app, conn, connect };
+module.exports = { app, connect };
--- admin/api/routes/accommodations/getAccommodationById.js
+++ admin/api/routes/accommodations/getAccommodationById.js
@@ -1,8 +1,8 @@
 const { app } = require('../../database/conn');
 
-module.exports = app.get('/accommodations', async (_, res) => {
+module.exports = app.get('/accommodations/:id', async (req, res) => {
   try {
-    const [rows] = await global.connection.query('SELECT * FROM accommodation;');
+    const [rows] = await global.connection.query(`SELECT * FROM accommodation WHERE id = ${req.params.id};`);
 
   if (!rows)
     return res.status(422).json({ message: 'Record not found!' });
