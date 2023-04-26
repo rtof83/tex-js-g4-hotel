368041ef215dac04a03cd36e473e193c8a1629c5
mauricio-alves <mauricioalvesnutri@gmail.com>
2023-04-26 18:29:42 -0300

    feature: added isAuth to create reservation

--- admin/api/routes/reservations/createReservation.js
+++ admin/api/routes/reservations/createReservation.js
@@ -1,22 +1,25 @@
-const { app } = require('../../database/conn');
+const { app } = require("../../database/conn");
+const isAuth = require("../../middlewares/isAuth");
 
-const Reservation = require('../../models/Reservation');
-const ReservationServices = require('../../models/ReservationServices');
+const Reservation = require("../../models/Reservation");
+const ReservationServices = require("../../models/ReservationServices");
 
-module.exports = app.post('/reservations', async (req, res) => {
+module.exports = app.post("/reservations", isAuth, async (req, res) => {
   try {
     let lastId = 0;
-    await Reservation.create(req.body)
-      .then(result => lastId = result.id);
+    await Reservation.create(req.body).then((result) => (lastId = result.id));
 
     if (req.body.services.length) {
       req.body.services.map(async (service) => {
-        await ReservationServices.create({ reservationId: lastId, serviceId: service.id })
+        await ReservationServices.create({
+          reservationId: lastId,
+          serviceId: service.id,
+        });
       });
-    };
-      
-    res.status(200).json({ message: 'Record created successfully' });
+    }
+
+    res.status(200).json({ message: "Record created successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
-  };
+  }
 });
